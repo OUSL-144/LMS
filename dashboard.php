4367540691636d614644c84a5dca54a79fba2f7a
uvinda948 <59842384+uvinda948@users.noreply.github.com>
2022-12-02 21:33:17 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -7,14 +7,10 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>Online Library Management System | User Dash Board</title>
-    <!-- BOOTSTRAP CORE STYLE  -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet" />
-    <!-- FONT AWESOME STYLE  -->
-    <link href="assets/css/font-awesome.css" rel="stylesheet" />
-    <!-- CUSTOM STYLE  -->
-    <link href="assets/css/style.css" rel="stylesheet" />
-    <!-- GOOGLE FONT -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+     <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <link href="assets/css/style.css" rel="stylesheet" />
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
 
 </head>
 <body>
